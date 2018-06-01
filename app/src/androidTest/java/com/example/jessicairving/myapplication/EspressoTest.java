3b6e849e157116a066d471d70450fda2a0f3ad2d
Jessica Irving <jessicairving@Jessicas-MacBook-Air.local>
2018-06-01 14:28:59 -0300

    Tests for UI using espresso

---
+++ app/src/androidTest/java/com/example/jessicairving/myapplication/EspressoTest.java
@@ -0,0 +1,41 @@
+package com.example.jessicairving.myapplication;
+
+import org.junit.runner.RunWith;
+import org.junit.Rule;
+import org.junit.Test;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+
+@RunWith(AndroidJUnit4.class)
+
+public class EspressoTest {
+
+    public static String testString = "Example123";
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void changeUserInputText() {
+        //Write the password in the input field
+        onView(withId(R.id.userInputField)).perform(typeText(testString));
+
+        //Check the text matches what the user typed in
+        onView(withId(R.id.userInputField)).check(matches(withText(testString)));
+
+        //Click the button
+        onView(withId(R.id.button)).perform(click());
+    }
+
+    public void passwordStrengthDisplay() {
+        onView(withID(R.id.strengthDisplay)).check(matches(withText("5")));
+    }
+
+}
