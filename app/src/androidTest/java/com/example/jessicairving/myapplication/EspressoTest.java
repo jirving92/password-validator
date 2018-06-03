46534f13f5e8a9c57e6e37bdee43c6eadd30e527
Jessica Irving <jessicairving@Jessicas-MacBook-Air.local>
2018-06-03 13:22:32 -0300

    All tests are now passing, added comments, changed default value for strength display

--- app/src/androidTest/java/com/example/jessicairving/myapplication/EspressoTest.java
+++ app/src/androidTest/java/com/example/jessicairving/myapplication/EspressoTest.java
@@ -18,12 +18,13 @@ public class EspressoTest {
 
     public static String testString = "Example123";
     public static String testString2 = "password";
-    public static String testString3 = "example123";
+    public static String testString3 = "example1";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
+    //Enters a password, submits, and looks for a display that says t is a strong password
     @Test
     public void strongPasswordTest() {
         //Write the password in the input field
@@ -39,5 +40,36 @@ public class EspressoTest {
 
     }
 
+    //Enters a password, submits, and looks for a display that says t is a weak password
+    @Test
+    public void weakPasswordTest() {
+        //Write the password in the input field
+        onView(withId(R.id.userInputField)).perform(typeText(testString2));
+
+        //Check the text matches what the user typed in
+        onView(withId(R.id.userInputField)).check(matches(withText(testString2)));
+
+        //Click the button
+        onView(withId(R.id.button)).perform(click());
+
+        onView(withId(R.id.strengthDisplay)).check(matches(withText("Your password is weak")));
+
+    }
+
+    //Enters a password, submits, and looks for a display that says t is an okay password
+    @Test
+    public void okayPasswordTest() {
+        //Write the password in the input field
+        onView(withId(R.id.userInputField)).perform(typeText(testString3));
+
+        //Check the text matches what the user typed in
+        onView(withId(R.id.userInputField)).check(matches(withText(testString3)));
+
+        //Click the button
+        onView(withId(R.id.button)).perform(click());
+
+        onView(withId(R.id.strengthDisplay)).check(matches(withText("Your password is okay")));
+
+    }
 
 }
